ef31f5df773d8dc946e7883bc174cabd297199f7
Lobatskiy Andriy <lobatskiy31@gmail.com>
2020-12-04 12:23:55 +0200

    magento 2

---
+++ app/etc/env.php
@@ -0,0 +1,54 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_12cue5'
+    ],
+    'crypt' => [
+        'key' => '39fe034ad5ad3b549afff1a250503e33'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'admin',
+                'password' => 'admin123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Wed, 02 Dec 2020 19:08:03 +0000'
+    ]
+];
